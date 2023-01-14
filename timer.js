d76fdd9cb90b5e54d1a34098e3307c0924aa9b80
wlsh44 <k87913j@gmail.com>
2023-01-14 22:02:34 +0900

    refactor: 코드 정리

--- timer.js
+++ timer.js
@@ -1,54 +1,76 @@
-const END_MESSAGE = "종료";
-const OVER_LIMIT_TIME_ALERT = "그만";
-const LIMIT_TIME = 60 * 99;
 const ONE_MINUTE = 60;
 const THREE_MINUTE = 180;
 const FIVE_MINUTE = 300;
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
-let timer = document.getElementById("timer");
 let time = 0;
 let playing = false;
+let timerEventId;
+
+startBtn.addEventListener("click", startEvent);
+stopBtn.addEventListener("click", stopEvent);
+document.getElementById("1min").addEventListener("click", function () {
+    addTime(ONE_MINUTE);
+});
+
+document.getElementById("3min").addEventListener("click", function () {
+    addTime(THREE_MINUTE);
+});
+
+document.getElementById("5min").addEventListener("click", function () {
+    addTime(FIVE_MINUTE);
+});
+
+function startEvent() {
+    if (playing) {
+        return;
+    }
+    playing = true;
+
+    timerEventId = setInterval(function () {
+        if (time <= 0) {
+            playing = false;
+            finishTimer();
+            return;
+        }
+        time--;
+        showTimer();
+    }, 1000);
+}
+
+function finishTimer() {
+    const END_MESSAGE = "종료";
+
+    clearInterval(timerEventId);
+    setTimeout(function () {
+        alert(END_MESSAGE)
+    });
+}
 
 function showTimer() {
-    min = parseInt(time / 60);
+    let timer = document.getElementById("timer");
+    let min = parseInt(time / 60);
+    let sec = time % 60;
+
     if (min < 10) {
         min = "0" + min;
     }
-    sec = time % 60;
     if (sec < 10) {
         sec = "0" + sec;
     }
     timer.innerText = min + ":" + sec;
 }
 
-startBtn.addEventListener("click", function() {
-    if (playing) {
-        return;
-    }
-    playing = true;
-
-    startId = setInterval(function () {
-        if (time <= 0) {
-            playing = false;
-            clearInterval(startId);
-            setTimeout(function () {
-                alert(END_MESSAGE)
-            });
-            return;
-        }
-        time--;
-        showTimer();
-    }, 1000);
-})
-
-stopBtn.addEventListener("click", function () {
+function stopEvent() {
     playing = false;
-    clearInterval(startId);
-})
+    clearInterval(timerEventId);
+}
+
 
 function addTime(addTime) {
-    if (!validateTime(addTime)) {
+    const OVER_LIMIT_TIME_ALERT = "그만";
+
+    if (!validateLimitTime(addTime)) {
         alert(OVER_LIMIT_TIME_ALERT);
         return;
     }
@@ -56,21 +78,7 @@ function addTime(addTime) {
     showTimer();
 }
 
-function validateTime(addTime) {
-    if (time + addTime > LIMIT_TIME) {
-        return false;
-    }
-    return true;
+function validateLimitTime(addTime) {
+    const limitTime = 60 * 99;
+    return time + addTime <= limitTime;
 }
-
-document.getElementById("1min").addEventListener("click", function() {
-    addTime(ONE_MINUTE);
-})
-
-document.getElementById("3min").addEventListener("click", function() {
-    addTime(THREE_MINUTE);
-})
-
-document.getElementById("5min").addEventListener("click", function() {
-    addTime(FIVE_MINUTE);
-})
